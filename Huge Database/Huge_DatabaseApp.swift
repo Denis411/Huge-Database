1263468b8089bb6fd5977e0886b4ef0f775aa2ca
Denis411 <81258539+Denis411@users.noreply.github.com>
2021-06-13 19:16:55 +0300

    Initial Commit

---
+++ Huge Database/Huge_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Huge_DatabaseApp.swift
+//  Huge Database
+//
+//  Created by Programmer on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Huge_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
